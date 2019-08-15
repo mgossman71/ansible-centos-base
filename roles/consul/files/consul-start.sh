7b2c6c03177fa08f42b1d9e860eab51b216e49a7
Mark.Gossman <mark.gossman@target.com>
2019-08-15 15:57:55 -0500

    update

---
+++ roles/consul/files/consul-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/usr/local/bin/consul agent \
+-bind=$(/sbin/ifconfig $(/bin/netstat -rn |/bin/grep "^0.0.0.0"|/bin/awk '{print $8}') |/bin/grep -w  "inet"|/bin/awk '{print $2}') \
+-client=0.0.0.0 \
+-config-file /usr/local/etc/consul.json \
+-config-file /usr/local/etc/postgres.json \
+-config-file /usr/local/etc/grafana.json
