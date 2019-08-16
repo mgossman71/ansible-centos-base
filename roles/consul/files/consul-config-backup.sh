6dcb7323557bca92facc28d81f5357a0d85693c9
gozz <gozz@gozzs-air.lan>
2019-08-16 07:09:53 -0500

    update

---
+++ roles/consul/files/consul-config-backup.sh
@@ -0,0 +1,15 @@
+cd /etc/systemd/system
+for i in $(ls|grep consul)
+do
+  consul kv put $(hostname)$(pwd)/$i \@$i
+done
+
+cd /usr/local/etc/
+for i in $(ls)
+  do consul kv put $(hostname)$(pwd)/$i \@$i
+done
+
+cd /usr/local/bin/
+for i in $(ls|grep start)
+  do consul kv put $(hostname)$(pwd)/$i \@$i
+done
